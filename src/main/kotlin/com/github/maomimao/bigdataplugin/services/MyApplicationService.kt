49304c5fbd02bcfe4932f486494d569da615ea01
GitHub Action <action@github.com>
2022-01-12 02:45:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/maomimao/bigdataplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/maomimao/bigdataplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.maomimao.bigdataplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maomimao.bigdataplugin.MyBundle
 
 class MyApplicationService {
 
